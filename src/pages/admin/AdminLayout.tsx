cecd284d9bc459c5ca6f2f47288895b22d866396
ChrisHudson68 <nathanhsocial@gmail.com>
2026-03-16 12:57:45 -0400

    Add favicon to admin portal

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -369,6 +369,8 @@ export const AdminLayout: FC<AdminLayoutProps> = ({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{appName} Platform Admin</title>
+        <link rel="stylesheet" href="/static/css/app.css" />
+        <link rel="icon" type="image/png" href="/static/favicon.png" />
         <style dangerouslySetInnerHTML={{ __html: adminCss }} />
       </head>
       <body>
